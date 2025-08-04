ccfe49facd1beca312862d5101c9fd49e2288c15
Damaroo <levkichigin@yandex.ru>
2025-08-04 17:15:05 +0300

    fix(tooltip): hover blink

--- src/widgets/ui/tooltip.tsx
+++ src/widgets/ui/tooltip.tsx
@@ -1,11 +1,13 @@
+import { cn } from '@/shared/lib/utils'
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from '@/shared/ui'
+import { Portal as RadixTooltipPortal } from '@radix-ui/react-tooltip'
 
-interface Props {
+interface TooltipWidgetProps {
   children: React.ReactNode
   label: string
   side?: 'top' | 'right' | 'bottom' | 'left'
@@ -20,14 +22,22 @@ export function TooltipWidget({
   align = 'center',
   delayDuration = 250,
   className,
-}: Props) {
+}: TooltipWidgetProps) {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={delayDuration}>
-        <TooltipTrigger className={className} asChild>{children}</TooltipTrigger>
-        <TooltipContent side={side} align={align}>
-          <p className="font-semibold text-sm">{label}</p>
-        </TooltipContent>
+        <TooltipTrigger className={cn('', className)} asChild>
+          {children}
+        </TooltipTrigger>
+        <RadixTooltipPortal>
+          <TooltipContent
+            side={side}
+            align={align}
+            style={{ pointerEvents: 'none' }}
+          >
+            <p className="font-semibold text-sm">{label}</p>
+          </TooltipContent>
+        </RadixTooltipPortal>
       </Tooltip>
     </TooltipProvider>
   )
